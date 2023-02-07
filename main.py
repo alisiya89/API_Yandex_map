fe767a3ecec51388bd2ce1914d2ed7876cace508
w0nkest <nmdvoret@gmail.com>
2023-02-07 16:07:13 +0900

    sheets change added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 import requests
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
+import requests
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PyQt5.QtGui import QPixmap, QIcon
 
 
 class Window(QMainWindow):
@@ -14,6 +14,25 @@ class Window(QMainWindow):
         self.setGeometry(700, 100, 700, 550)
         self.setWindowTitle('Работа с картой')
 
+        self.sheet_scheme = QPushButton('', self)
+        self.sheet_scheme.resize(30, 30)
+        self.sheet_scheme.move(50, 450)
+        self.sheet_scheme.setIcon(QIcon('mapicon.png'))
+
+        self.sheet_spn = QPushButton('', self)
+        self.sheet_spn.resize(30, 30)
+        self.sheet_spn.move(80, 450)
+        self.sheet_spn.setIcon(QIcon('spnicon.png'))
+
+        self.sheet_hybrid = QPushButton('', self)
+        self.sheet_hybrid.resize(30, 30)
+        self.sheet_hybrid.move(110, 450)
+        self.sheet_hybrid.setIcon(QIcon('hybridicon.png'))
+
+        self.sheet_scheme.clicked.connect(self.scheme)
+        self.sheet_spn.clicked.connect(self.spn)
+        self.sheet_hybrid.clicked.connect(self.hybrid)
+
         self.map = QLabel(self)
         self.map.move(50, 30)
         self.map.resize(400, 400)
@@ -21,10 +40,22 @@ class Window(QMainWindow):
         pixmap = pixmap.scaled(400, 400)
         self.map.setPixmap(pixmap)
 
-        self.map_ll = [87.3, 55.7]
+        self.map_ll = [0, 40]
         self.map_l = 'map'
         self.map_zoom = 7
 
+        self.refresh_map()
+
+    def hybrid(self):
+        self.map_l = 'sat,skl'
+        self.refresh_map()
+
+    def spn(self):
+        self.map_l = 'sat'
+        self.refresh_map()
+
+    def scheme(self):
+        self.map_l = 'map'
         self.refresh_map()
 
     def refresh_map(self):
@@ -40,8 +71,10 @@ class Window(QMainWindow):
         pixmap.load('map.png')
         self.map.setPixmap(pixmap)
 
+
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     sys.excepthook = except_hook
